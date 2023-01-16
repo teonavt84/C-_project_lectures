4babcab4a7d852afc5fa2836f2ee0cd5c7c53280
Denis <denis.volobuev1984@yandex.ru>
2023-01-16 23:26:47 +0300

    Add Example009v2

---
+++ Example010_IntroArray_v2/Program.cs
@@ -0,0 +1,44 @@
+﻿//В одномерном массиве array из n элементов требуется найти элемент массива, который равен find.
+//Решение с участием генератора случайных чисел и методов.
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10];
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
